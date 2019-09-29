b4def216417c2b4bcabcc3f47c341488a8d9d83c
berlangerc <cedric.berlanger@outlook.com>
2019-09-29 11:13:33 +0200

    Failing test for decreasing items where sellIn has passed

--- TypeScript/test/types/DecreasingItem.spec.ts
+++ TypeScript/test/types/DecreasingItem.spec.ts
@@ -19,4 +19,21 @@ describe('Decreasing Item', function () {
             });
         });
     });
+
+    describe('Updating quality when sellIn has been passed', () => {
+
+        const items = [
+            { item: new DecreasingItem("decrease1", 0, 2), expectedQuality: 0 },
+            { item: new DecreasingItem("decrease1", -10, 10), expectedQuality: 8 }
+        ];
+
+        items.forEach(({ item, expectedQuality }) => {
+            describe(`When the quality is updated for item ${item.name} with quality ${item.quality} and sellIn ${item.sellIn}`, () => {
+                it(`The quality is decreased with 1 and equals to ${expectedQuality}`, function () {
+                    item.update();
+                    expect(item.quality).to.equal(expectedQuality);
+                });
+            });
+        });
+    });
 });
